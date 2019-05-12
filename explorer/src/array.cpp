5443de53afe2946786991ebbf0f54bca255e8ec9
shaoc1 <changrui.shao@emc.com>
2019-05-12 17:13:10 +0800

    [Leetcode 229] complete a new solution to this problem using Boyer-Moore vote algorithm

--- explorer/src/array.cpp
+++ explorer/src/array.cpp
@@ -3470,5 +3470,47 @@ int majorityElement(vector<int>& nums)
  */
 vector<int> majorityElement3(vector<int>& nums)
 {
-    
+    if (nums.empty()){
+        return vector<int>{};
+    }
+    // get two possible candidates
+    vector<int> result;
+    int count1 = 0, count2 = 0;
+    int candidate1 = 0, candidate2 = 1;
+    for(auto& num: nums) {
+        if(candidate1 == num) {
+            count1 += 1;
+        } else if(candidate2 == num) {
+            count2 += 1;
+        } else if(count1 == 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if(count2 == 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1 -= 1;
+            count2 -= 1;
+        }
+    }
+    // get # of candidates
+    count1 = 0;
+    count2 = 0;
+    for(auto& num: nums) {
+        if(num == candidate1) {
+            count1 += 1;
+        }
+        if(num == candidate2) {
+            count2 += 1;
+        }
+    }
+    // judge if the candidate is correct
+    if(count1 > nums.size() / 3) {
+        result.push_back(candidate1);
+    }
+    if(count2 > nums.size() / 3) {
+        result.push_back(candidate2);
+    }
+
+    return result;
 }
